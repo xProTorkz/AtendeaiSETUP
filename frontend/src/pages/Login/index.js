26616b03fd1da587b515ad5ae3c84360dd7dda28
xProTorkz <127126801+xProTorkz@users.noreply.github.com>
2025-04-28 19:42:39 -0300

    Codigo Base
    Base

---
+++ frontend/src/pages/Login/index.js
@@ -0,0 +1,139 @@
+import React, { useState, useContext } from "react";
+import { Link as RouterLink } from "react-router-dom";
+
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles, useTheme } from "@material-ui/core/styles"; // Importe useTheme
+import Container from "@material-ui/core/Container";
+
+import { i18n } from "../../translate/i18n";
+
+import { AuthContext } from "../../context/Auth/AuthContext";
+import logo from "../../assets/logo.png";
+import logoWhite from "../../assets/logo_branca.png"; // Importe a logo branca
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100vw",
+    height: "100vh",
+    backgroundColor: theme.palette.type === 'dark' ? theme.palette.background.default : '#f5f5f5', // Define a cor de fundo com base no tema
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    textAlign: "center",
+  },
+  paper: {
+    backgroundColor: theme.palette.login,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    padding: "55px 30px",
+    borderRadius: "12.5px",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+  powered: {
+    color: "white"
+  }
+}));
+
+const Login = () => {
+  const classes = useStyles();
+  const theme = useTheme(); // Adicione useTheme aqui
+
+  const [user, setUser] = useState({ email: "", password: "" });
+
+  const { handleLogin } = useContext(AuthContext);
+
+  const handleChangeInput = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  const handlSubmit = (e) => {
+    e.preventDefault();
+    handleLogin(user);
+  };
+
+  return (
+    <div className={classes.root}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline/>
+        <div className={classes.paper}>
+          <div>
+            {/* Altera a logo com base no tema */}
+            <img style={{ margin: "0 auto", width: "100%" }} src={theme.palette.type === 'dark' ? logoWhite : logo} alt="Whats" />
+          </div>
+          {/*<Typography component="h1" variant="h5">
+            {i18n.t("login.title")}
+          </Typography>*/}
+          <form className={classes.form} noValidate onSubmit={handlSubmit}>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label={i18n.t("login.form.email")}
+              name="email"
+              value={user.email}
+              onChange={handleChangeInput}
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label={i18n.t("login.form.password")}
+              type="password"
+              id="password"
+              value={user.password}
+              onChange={handleChangeInput}
+              autoComplete="current-password"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              {i18n.t("login.buttons.submit")}
+            </Button>
+            { <Grid container>
+              <Grid item>
+                <Link
+                  href="#"
+                  variant="body2"
+                  component={RouterLink}
+                  to="/signup"
+                >
+                  {i18n.t("login.buttons.register")}
+                </Link>
+              </Grid>
+            </Grid> }
+          </form>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default Login;
